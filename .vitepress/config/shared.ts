bba349096e03fff7fe2ce6375e235ea503b93488
linpengteng <china.linpengteng@gmail.com>
2025-06-29 02:07:19 +0800

    chore: enable algolia search

--- .vitepress/config/shared.ts
+++ .vitepress/config/shared.ts
@@ -17,6 +17,7 @@ export const shared = defineConfig({
     ['meta', { property: 'og:site_name', content: 'Antdv.pro' }],
     ['meta', { property: 'og:title', content: 'Antdv.pro | Backend Management System Template' }],
     ['meta', { property: 'og:image', content: 'https://antdvr.github.io/resources/images/Antdvr-3.x.png' }],
+    ['meta', { name: 'algolia-site-verification', content: 'B6571016E319A480' }],
     ['link', { rel: 'icon', type: 'image/svg+xml', href: `${base}/logo-mini.svg` }],
     ['link', { rel: 'icon', type: 'image/png', href: `${base}/logo-mini.png` }],
   ],
@@ -31,6 +32,57 @@ export const shared = defineConfig({
     socialLinks: [
       { icon: 'github', link: `${base}/redirect.html?\${domain}.com/antdvr/vue-template-3.x` },
     ],
+
+    search: {
+      provider: 'algolia',
+      options: {
+        appId: '460CT3LJ0M',
+        apiKey: 'b512a39bda80881bcf3ddecb0531e51d',
+        indexName: 'crawler_Antdvr',
+        locales: {
+          root: {
+            placeholder: '搜索文档',
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档',
+              },
+              modal: {
+                searchBox: {
+                  resetButtonTitle: '清除查询条件',
+                  resetButtonAriaLabel: '清除查询条件',
+                  cancelButtonText: '取消',
+                  cancelButtonAriaLabel: '取消',
+                },
+                startScreen: {
+                  recentSearchesTitle: '搜索历史',
+                  noRecentSearchesText: '没有搜索历史',
+                  saveRecentSearchButtonTitle: '保存至搜索历史',
+                  removeRecentSearchButtonTitle: '从搜索历史中移除',
+                  favoriteSearchesTitle: '收藏',
+                  removeFavoriteSearchButtonTitle: '从收藏中移除',
+                },
+                errorScreen: {
+                  titleText: '无法获取结果',
+                  helpText: '你可能需要检查你的网络连接',
+                },
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换',
+                  closeText: '关闭',
+                },
+                noResultsScreen: {
+                  noResultsText: '无法找到相关结果',
+                  suggestedQueryText: '你可以尝试查询',
+                  reportMissingResultsText: '你认为该查询应该有结果？',
+                  reportMissingResultsLinkText: '点击反馈',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
   },
 
   sitemap: {
